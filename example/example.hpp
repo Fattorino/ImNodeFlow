b42f18803fa71012493c0a18c315ac0b6d41868d
Fattorino <gabriele.torelli.29@gmail.com>
2025-06-09 09:19:40 +0200

    Updated example

--- example/example.hpp
+++ example/example.hpp
@@ -1,29 +1,40 @@
-/**
- * This is a minimal implementation of the Simple Sum example
- */
 #pragma once
 
 #include "ImNodeFlow.h"
 
-/* The simple sum basic node */
-class SimpleSum : public ImFlow::BaseNode
-{
-
+class SimpleSum : public ImFlow::BaseNode {
 public:
-    SimpleSum()
-    {
+    SimpleSum() {
         setTitle("Simple sum");
         setStyle(ImFlow::NodeStyle::green());
         ImFlow::BaseNode::addIN<int>("In", 0, ImFlow::ConnectionFilter::SameType());
-        ImFlow::BaseNode::addOUT<int>("Out", nullptr)->behaviour([this](){ return getInVal<int>("In") + m_valB; });
+        ImFlow::BaseNode::addOUT<int>("Out", nullptr)->behaviour([this]() { return getInVal<int>("In") + m_valB; });
     }
 
-    void draw() override
-    {
+    void draw() override {
+        ImGui::SetNextItemWidth(100.f);
+        ImGui::InputInt("##ValB", &m_valB);
+    }
+
+private:
+    int m_valB = 0;
+};
+
+class CollapsingNode : public ImFlow::BaseNode {
+public:
+    CollapsingNode() {
+        setTitle("Collapsing node");
+        setStyle(ImFlow::NodeStyle::red());
+        ImFlow::BaseNode::addIN<int>("In", 0, ImFlow::ConnectionFilter::SameType());
+        ImFlow::BaseNode::addIN<int>("Other", 0, ImFlow::ConnectionFilter::SameType());
+        ImFlow::BaseNode::addOUT<int>("Out", nullptr)->behaviour([this]() { return getInVal<int>("In") + m_valB; });
+    }
+
+    void draw() override {
         if(ImFlow::BaseNode::isSelected()) {
-          ImGui::SetNextItemWidth(100.f);
-          ImGui::InputInt("##ValB", &m_valB);
-          ImGui::Button("Hello");
+            ImGui::Text("You can only see me when the node is selected!");
+            ImGui::SetNextItemWidth(100.f);
+            ImGui::InputInt("##ValB", &m_valB);
         }
     }
 
@@ -32,26 +43,22 @@ private:
 };
 
 /* Node editor that sets up the grid to place nodes */
-struct NodeEditor : ImFlow::BaseNode
-{
+struct NodeEditor : ImFlow::BaseNode {
     ImFlow::ImNodeFlow mINF;
-    NodeEditor(float d, std::size_t r)
-    : BaseNode()
-    {
-        mINF.setSize({d,d});
-        if(r > 0) {
-          mINF.addNode<SimpleSum>({0,0});
-          mINF.addNode<SimpleSum>({10,10});
+
+    NodeEditor(float d, std::size_t r) : BaseNode() {
+        mINF.setSize({d, d});
+        if (r > 0) {
+            mINF.addNode<SimpleSum>({0, 0});
+            mINF.addNode<SimpleSum>({10, 10});
         }
     }
 
-    void set_size(ImVec2 d)
-    {
+    void set_size(ImVec2 d) {
         mINF.setSize(d);
     }
 
-    void draw() override
-    {
+    void draw() override {
         mINF.update();
     }
 };
